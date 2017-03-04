b40891f209a72588639281c481b743f02c7e3230
Angelo Ocana <alanmarcell@live.com>
2017-03-04 17:37:01 +0000

    Typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,5 @@
-/// <reference path="modules/sinon/index.d.ts" />
 /// <reference path="../node_modules/ptz-core-domain/src/typings/Index.d.ts" />
+/// <reference path="modules/bcryptjs/index.d.ts" />
+/// <reference path="modules/dotenv/index.d.ts" />
+/// <reference path="modules/shortid/index.d.ts" />
+/// <reference path="modules/sinon/index.d.ts" />
