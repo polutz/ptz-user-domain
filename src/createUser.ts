d035bc31117a9043e591009ef6bacc926ac21a17
Alan Marcell <alanmarcell@live.com>
2017-06-28 15:53:17 -0300

    CreateUser with new ptz-validations

--- src/createUser.ts
+++ src/createUser.ts
@@ -1,31 +1,57 @@
-import * as V from 'ptz-validations';
+import * as V from '@alanmarcell/ptz-validations';
 import { IUser } from './IUser';
 
-export const userNameValidation = V.validateString({
-    required: true,
-    minLength: 3,
-    maxLength: 30,
-    toLowerCase: true
-});
+// export const userNameValidation = V.validateString({
+//     required: true,
+//     minLength: 3,
+//     maxLength: 30,
+//     toLowerCase: true
+// });
 
-export const getPasswordValidation = (required: boolean) => V.validateString({
-    required,
-    minLength: 6,
-    maxLength: 30
-});
+// export const getPasswordValidation = (required: boolean) => V.validateString({
+//     required,
+//     minLength: 6,
+//     maxLength: 30
+// });
 
 /**
  * Create user
  */
 export const createUser = V.validate<IUser>({
-    displayName: V.validateString({
-        required: true,
-        minLength: 2,
-        maxLength: 50
-    }),
-    userName: userNameValidation,
-    password: getPasswordValidation(false),
-    email: V.validateEmail({
-        required: true
-    })
+    id: [
+        V.generateId
+    ],
+    displayName: [
+        V.required,
+        V.isString,
+        V.min(2),
+        V.max(100)
+    ],
+    userName: [
+        V.required,
+        V.isString,
+        V.min(2),
+        V.max(40),
+        V.toLowerCase
+    ],
+    password: [
+        V.required,
+        V.isString,
+        V.min(6),
+        V.max(40)
+    ],
+    email: [
+        V.required,
+        V.isEmail
+    ],
+    weight: [
+        V.isNumber,
+        V.min(1),
+        V.max(1000)
+    ],
+    birthday: [
+        V.isDate,
+        V.min(new Date('1800-01-01')),
+        V.max(new Date())
+    ]
 });
